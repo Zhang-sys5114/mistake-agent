25a36a4eb33d98fec751bf6aa9587a0a7762f28a
Zhang-sys5114 <zh7225035114@gmail.com>
2026-08-09 08:32:37 +0800

    修 generate.rs 导入

--- src/plugin/practice/generate.rs
+++ src/plugin/practice/generate.rs
@@ -5,7 +5,7 @@
 
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use serde_json::{Value, json};
+use serde_json::Value;
 
 use crate::kernel::contract::ToolError;
 use crate::kernel::message::Message;
